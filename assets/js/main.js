2dc0808823d2573fd11df63bbf7860e5e1658cc5
Clem-Coding <clementine.blondeau@gmail.com>
2024-10-21 21:45:43 +0200

    commit v2

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,8 +6,15 @@ let board = document.querySelector(".board-container");
 let generateGridBtn = document.querySelector(".js-generate-grid");
 let colorPickers = document.querySelectorAll(".color-input");
 let clearBtn = document.querySelector(".clear-btn");
+let glitchBtn = document.querySelector(".glitch-btn");
+let eraserBtn = document.querySelector(".eraser-btn");
+let brushBtn = document.querySelector(".brush-btn");
 
-console.log(clearBtn);
+let MouseDown = false;
+let eraserSelected = false;
+let brushSelected = false;
+
+console.log("le bouton paint", brushBtn);
 
 /**************************************************************/
 /*                         Fonctions                            */
@@ -18,21 +25,12 @@ console.log(clearBtn);
 function generateGrid(gridSize = 10) {
   // Valeur par défaut de 10x10
 
-  console.log("je teste la grille");
-
   gridSize = parseInt(document.getElementById("grid-size").value);
-  console.log("Taille de la grille:", gridSize);
-
-  // Réinitialiser le contenu de la grille
+  const totalBoxes = gridSize * gridSize;
   board.innerHTML = "";
 
-  // Définir le nombre de cases
-  const totalBoxes = gridSize * gridSize;
-
-  // Mettre à jour le style de la grille
   board.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
 
-  // crée les cases
   for (let i = 0; i < totalBoxes; i++) {
     const boardBoxes = document.createElement("div");
     boardBoxes.className = "board-boxes";
@@ -43,9 +41,8 @@ function generateGrid(gridSize = 10) {
 // ____________________ Fonctions Local Storage _______________________________
 function selectColor(event) {
   let colorPalette = event.target;
-  console.log("colorPalette", colorPalette);
 
-  let selectedColor = colorPalette.value; // Récupère la valeur sélectionnée
+  let selectedColor = colorPalette.value;
   console.log("selected color", selectedColor);
   sessionStorage.setItem("selectedColor", selectedColor); // Stocke la couleur dans sessionStorage
 }
@@ -57,10 +54,20 @@ function getSelectedColor() {
   return null; // Pourquoi null?
 }
 
-//_________ Fonction pour charger la couleur choisie dans la palette____________
+//___________ Fonctions pour le pinceau________________
+
+function colorOnCells() {
+  const boardBoxes = document.querySelectorAll(".board-boxes");
+
+  if (brushSelected) {
+    boardBoxes.forEach((box) => {
+      box.removeEventListener("mouseover", deleteColorOnCells);
+      box.addEventListener("mouseover", loadSelectedColor);
+    });
+  }
+}
 
 function loadSelectedColor(event) {
-  console.log("je commence à colorier");
   let cellMain = event.target;
   let selectedColor = getSelectedColor();
 
@@ -69,32 +76,33 @@ function loadSelectedColor(event) {
   }
 }
 
-//___________ Fonctions pour activer et désactiver le coloriage________________
-
-let MouseDown = false;
-
-function colorOnCells() {
-  const boardBoxes = document.querySelectorAll(".board-boxes");
-  MouseDown = true;
-
-  boardBoxes.forEach((box) => {
-    box.addEventListener("mouseover", loadSelectedColor);
-  });
+// ________ Fonctions pour la gomme ______________
+function deleteColorOnCells(event) {
+  let cellMain = event.target;
+  console.log("cell main fonction erase", cellMain);
+  if (MouseDown) {
+    cellMain.style.backgroundColor = "";
+  }
 }
 
-function stopColoring() {
+function erasePaint() {
   const boardBoxes = document.querySelectorAll(".board-boxes");
-  MouseDown = false;
 
-  boardBoxes.forEach((box) => {
-    box.removeEventListener("mouseover", loadSelectedColor);
-  });
+  if (eraserSelected) {
+    console.log("la gomme est sélectionnée");
+    boardBoxes.forEach((box) => {
+      box.removeEventListener("mouseover", loadSelectedColor);
+      box.addEventListener("mouseover", deleteColorOnCells);
+    });
+  }
 }
+
+//_________ Fonction pour clear le board_______________________
 
 function clearPaintBoard() {
   const boardBoxes = document.querySelectorAll(".board-boxes");
   boardBoxes.forEach((box) => {
-    box.style.backgroundColor = ""; // Réinitialise la couleur de fond
+    box.style.backgroundColor = "";
   });
 }
 
@@ -103,15 +111,50 @@ function clearPaintBoard() {
 /**************************************************************/
 
 window.addEventListener("DOMContentLoaded", function () {
-  generateGrid();
-  generateGridBtn.addEventListener("click", generateGrid);
+  generateGrid(); // affiche la grille par défaut
+  generateGridBtn.addEventListener("click", () => {
+    generateGrid();
+    colorOnCells();
+  });
 
   colorPickers.forEach((input) => {
     input.addEventListener("input", selectColor);
   });
 
-  document.addEventListener("mousedown", colorOnCells);
-  document.addEventListener("mouseup", stopColoring);
+  document.addEventListener("mousedown", function () {
+    console.log("la souris est enfoncée");
+    MouseDown = true;
+  });
 
-  clearBtn.addEventListener("click", clearPaintBoard);
+  document.addEventListener("mouseup", function () {
+    console.log("la souris est relevée");
+    MouseDown = false;
+  });
+
+  eraserBtn.addEventListener("click", () => {
+    eraserBtn.classList.toggle("active");
+    brushBtn.classList.remove("active");
+    eraserSelected = true;
+    brushSelected = false;
+    erasePaint();
+  });
+
+  brushBtn.addEventListener("click", () => {
+    brushBtn.classList.toggle("active");
+    eraserBtn.classList.remove("active");
+    brushSelected = true;
+    eraserSelected = false;
+    colorOnCells();
+  });
 });
+
+glitchBtn.addEventListener("click", () => {
+  glitchBtn.classList.toggle("active");
+  const boardBoxes = document.querySelectorAll(".board-boxes");
+
+  boardBoxes.forEach((cell) => {
+    cell.classList.toggle("glitch");
+  });
+});
+
+clearBtn.addEventListener("click", clearPaintBoard);
